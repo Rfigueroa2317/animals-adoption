391d3894d00a849f5539dcce9d26a8dd1706c1de
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-28 10:44:36 -0500

    update

--- animalsAdoption.sql
+++ animalsAdoption.sql
@@ -4,6 +4,16 @@ CREATE DATABASE adoptionCenter;
 
 USE adoptionCenter;
 
-GRANT ALL ON adoptionCenter * TO 'root'@'localhost';
+GRANT ALL ON adoptionCenter. * TO 'root'@'localhost';
 
-CREATE TABLE
+CREATE TABLE animals (
+    animal_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    type VARCHAR(100) NOT NULL,
+    age INT,
+    size TEXT,
+    is_adoptable TINYINT(1),
+    is_cute TINYINT(1),
+    PRIMARY KEY (animal_id)
+);
+DESCRIBE animals;
