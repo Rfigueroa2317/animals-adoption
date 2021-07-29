484ae6548733ad308973afada7a31eade45867e5
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-29 08:49:44 -0500

    update

--- animalsAdoption.sql
+++ animalsAdoption.sql
@@ -33,32 +33,53 @@ INSERT INTO animals(name, type, age, size, is_adoptable, is_cute) VALUES
 
 SELECT * FROM adoptionCenter.animals;
 
-# WHERE Clause
+--  WHERE Clause
 SELECT name, type FROM animals WHERE type = 'dog';
 
-# WHERE - BETWEEN
+--  WHERE - BETWEEN
 SELECT name, age FROM animals WHERE age > 5 OR age < 10;
 SELECT name, age FROM animals WHERE age BETWEEN 5 and 10;
 SELECT name, type FROM animals WHERE type BETWEEN 'dog' AND 'parrot';
 
-# WHERE - LIKE: will conpare character by character, allows us to output
+--  WHERE - LIKE: will conpare character by character, allows us to output
 SELECT name FROM animals WHERE name LIKE '%y'; #animals ending with y
 SELECT name FROM animals WHERE type LIKE '%r%'; # animals whose type of
-# animal
-# has an r in it
+--  animal
+--  has an r in it
 
-# Where - DISTINCT : allows us to avoid duplicates
+--  Where - DISTINCT : allows us to avoid duplicates
 SELECT DISTINCT name FROM animals WHERE type = 'dog'; #this will pull
-# all distinct names as long as the animal is dog
+--  all distinct names as long as the animal is dog
 SELECT DISTINCT name, type FROM animals WHERE type = 'dog';
 SELECT DISTINCT type FROM animals;
 
-# WHERE - IN
+--  WHERE - IN
 SELECT * FROM animals;
 SELECT age FROM animals WHERE age IN (8, 3, 10, 4);
 SELECT name, type, age FROM animals WHERE type IN ('cat','dog','gerbil');
 
 
-# CHAINING WHERE $$ COMPARISON OPERATORS
+--  CHAINING WHERE $$ COMPARISON OPERATORS
 SELECT name FROM animals WHERE age > 6 AND type = 'dog';
 SELECT name, type FROM animals WHERE is_adoptable = 1 AND age < 5 AND type = 'cat';
+
+-- ORDER BY
+-- SELECT COLUMN FROM table ORDER BY column_name DESC
+
+-- output all animal names in descending age order
+SELECT name FROM animals ORDER BY age DESC;
+
+# output all animal names in descending age order, if the ages are between
+# 2 and 7
+SELECT name, age FROM animals WHERE age BETWEEN 2 and 7 ORDER BY age DESC;
+
+# output all animal names and types in ascending order by animal type
+SELECT name, type, age FROM animals ORDER BY type ASC;
+
+# LIMIT Clause - limits what we're getting back instead of getting too much data
+
+SELECT * FROM animals;
+SELECT name FROM animals WHERE type - 'dog' LIMIT 3 OFFSET 2;
+SELECT type FROM animals;
+
+# SELECT name, CONCAT(name,'the ',type, 'is up for adoption')
